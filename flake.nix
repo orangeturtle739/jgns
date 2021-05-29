b41e38de7b620590182b8ad77f442231644c9b48
Jacob Glueck <swimgiraffe435@gmail.com>
2021-05-28 17:37:41 -0700

    Switch to nixpkgs fork

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "jgns";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    # nixpkgs.url = "/home/jacob/git/nixpkgs";
+    nixpkgs.url = "github:orangeturtle739/nixpkgs/nixos-20.09";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
