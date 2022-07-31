97f1f8cbb8f4f26027589b66f4d0f966edc9b129
Jacob Glueck <swimgiraffe435@gmail.com>
2022-07-31 15:20:28 -0700

    Fix source code pro font

--- packages/source-code-pro-nerdfont/default.nix
+++ packages/source-code-pro-nerdfont/default.nix
@@ -2,18 +2,21 @@
 # https://github.com/NixOS/nixpkgs/pull/63079
 # https://github.com/NixOS/nixpkgs/issues/47921
 # Nerd fonts is 2.0 GB, so just download the font we need.
-{ lib, fetchzip }:
-let version = "2.1.0";
-in fetchzip {
-  name = "source-code-pro-nerdfont-${version}";
+{ stdenv, lib, fetchurl, unzip }:
+stdenv.mkDerivation rec {
+  version = "2.1.0";
+  pname = "source-code-pro-nerdfont";
+  src = fetchurl {
+    url =
+      "https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/SourceCodePro.zip";
+    sha256 = "p3FongvB0CDiCCxwXi+2ETt/j7wcVsY5lX8SVGvTlhk=";
+  };
+  nativeBuildInputs = [ unzip ];
 
-  url =
-    "https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/SourceCodePro.zip";
+  unpackPhase = ":";
 
-  postFetch = ''
+  installPhase = ''
     mkdir -p $out/share/fonts/source-code-pro-nerdfont
-    unzip -j $downloadedFile -d $out/share/fonts/source-code-pro-nerdfont
+    unzip -j $src -d $out/share/fonts/source-code-pro-nerdfont
   '';
-
-  sha256 = "13hgqmqb3sxsl9zwmz048c414fqfa1v066ji08airiw42jg0xz6d";
 }
